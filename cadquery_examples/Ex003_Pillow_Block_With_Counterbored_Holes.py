d115dc15f143fd2b5c4b588293eb38ead0aa8559
hiroshi sano <23502+hrsano645@users.noreply.github.com>
2024-06-22 17:11:05 +0900

    add exam comment

--- cadquery_examples/Ex003_Pillow_Block_With_Counterbored_Holes.py
+++ cadquery_examples/Ex003_Pillow_Block_With_Counterbored_Holes.py
@@ -1,31 +1,26 @@
 import cadquery as cq
 from ocp_vscode import show_object
 
-# These can be modified rather than hardcoding values for each dimension.
-length = 80.0  # Length of the block
-width = 100.0  # Width of the block
-thickness = 10.0  # Thickness of the block
-center_hole_dia = 22.0  # Diameter of center hole in block
-cbore_hole_diameter = 2.4  # Bolt shank/threads clearance hole diameter
-cbore_inset = 12.0  # How far from the edge the cbored holes are set
-cbore_diameter = 4.4  # Bolt head pocket hole diameter
-cbore_depth = 2.1  # Bolt head pocket hole depth
+# 各寸法の値をハードコーディングする代わりに、これらを変更できます。
+length = 80.0  # ブロックの長さ
+width = 100.0  # ブロックの幅
+thickness = 10.0  # ブロックの厚さ
+center_hole_dia = 22.0  # ブロックの中央の穴の直径
+cbore_hole_diameter = 2.4  # ボルトのシャンク/スレッドのクリアランス穴の直径
+cbore_inset = 12.0  # カウンターボアされた穴がエッジからどれだけ離れているか
+cbore_diameter = 4.4  # ボルトヘッドのポケット穴の直径
+cbore_depth = 2.1  # ボルトヘッドのポケット穴の深さ
 
-# Create a 3D block based on the dimensions above and add a 22mm center hold
-# and 4 counterbored holes for bolts
-# 1.  Establishes a workplane that an object can be built on.
-# 1a. Uses the X and Y origins to define the workplane, meaning that the
-#     positive Z direction is "up", and the negative Z direction is "down".
-# 2.  The highest(max) Z face is selected and a new workplane is created on it.
-# 3.  The new workplane is used to drill a hole through the block.
-# 3a. The hole is automatically centered in the workplane.
-# 4.  The highest(max) Z face is selected and a new workplane is created on it.
-# 5.  A for-construction rectangle is created on the workplane based on the
-#     block's overall dimensions.
-# 5a. For-construction objects are used only to place other geometry, they
-#     do not show up in the final displayed geometry.
-# 6.  The vertices of the rectangle (corners) are selected, and a counter-bored
-#     hole is placed at each of the vertices (all 4 of them at once).
+# 上記の寸法に基づいて3Dブロックを作成し、22mmの中央穴と4つのカウンターボアされたボルト穴を追加します。
+# 1. オブジェクトを構築できるワークプレーンを確立します。
+# 1a. XおよびYの原点を使用してワークプレーンを定義し、正のZ方向が「上」であり、負のZ方向が「下」であることを意味します。
+# 2. 最も高い（最大）のZ面が選択され、その上に新しいワークプレーンが作成されます。
+# 3. 新しいワークプレーンを使用してブロックを貫通する穴をドリルします。
+# 3a. 穴は自動的にワークプレーンの中心に配置されます。
+# 4. 最も高い（最大）のZ面が選択され、その上に新しいワークプレーンが作成されます。
+# 5. ブロックの全体的な寸法に基づいて、ワークプレーン上に構築用の長方形が作成されます。
+# 5a. 構築用オブジェクトは他のジオメトリを配置するためにのみ使用され、最終的に表示されるジオメトリには表示されません。
+# 6. 長方形の頂点（コーナー）が選択され、各頂点にカウンターボアされた穴が配置されます（4つすべての頂点に同時に）。
 result = (
     cq.Workplane("XY")
     .box(length, width, thickness)
@@ -38,8 +33,12 @@ result = (
     .vertices()
     .cboreHole(cbore_hole_diameter, cbore_diameter, cbore_depth)
     .edges("|Z")
-    .fillet(2.0)
+    .fillet(5.0)
 )
 
 # Displays the result of this script
 show_object(result, measure_tools=True)
+
+# forConstructionがいまいちわかってない: 補助ジオメトリはどこに配置されるのか。配置というよりは見えないけど配置されてるイメージ
+# verticesの意味が分かってない: 直訳すると頂点だがで、頂点のコーナーらしい
+# cboreHoleは？: カウンターボアされた穴を作成するメソッド。ボルトのシャンク/スレッドのクリアランス穴を作成するために使用される。
